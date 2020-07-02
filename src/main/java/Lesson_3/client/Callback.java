e21a44831c2e11c63647b16819bd434b43e27e22
screamcol <screamcol@gmail.com>
2020-07-02 14:51:32 +0300

    Client-Server for lesson_3

---
+++ src/main/java/Lesson_3/client/Callback.java
@@ -0,0 +1,5 @@
+package Lesson_3.client;
+
+public interface Callback {
+    void callback(Object... args);
+}
---
+++ src/main/java/Lesson_3/server/MainServer.java
@@ -0,0 +1,7 @@
+package Lesson_3.server;
+
+public class MainServer {
+    public static void main(String[] args) {
+        new Server();
+    }
+}
